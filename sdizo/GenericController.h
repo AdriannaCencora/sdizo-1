bff1f3b0ab6f74e028a1d79859b088636a184b75
Jakub Krzemiński <krzeminski.k@outlook.com>
2017-04-04 19:04:00 +0200

    Added generic controller

---
+++ sdizo/GenericController.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "stdafx.h"
+#include "GenericStructure.h"
+
+class GenericController
+{
+public:
+	virtual ~GenericController() {};
+	virtual void parseInput(std::string input) = 0;
+	virtual void printView() = 0;
+
+protected:
+	std::unique_ptr<GenericStructure> structure;
+};
+
