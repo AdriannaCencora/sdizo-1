673337b2f1d29bb8e10ab7e86294db4449cea6ac
Jakub Krzemiński <krzeminski.k@outlook.com>
2017-04-05 03:02:58 +0200

    Added display feature for list

--- sdizo/List.cpp
+++ sdizo/List.cpp
@@ -1,6 +1,32 @@
 #include "stdafx.h"
 #include "List.h"
 
+using namespace std;
+
+void List::printData()
+{
+	if (head == nullptr || tail == nullptr) 
+	{
+		cout << "Lista jest pusta" << endl;
+		return;
+	}
+	
+	Node* tmp = head.get;
+
+	while (tmp->next != nullptr)
+	{
+		cout << tmp->data << " ";
+	}
+	cout << endl;
+
+	Node* tmp = tail.get;
+
+	while (tmp->prev != nullptr)
+	{
+		cout << tmp->data << " ";
+	}
+}
+
 void List::addElement(int value)
 {
 }
