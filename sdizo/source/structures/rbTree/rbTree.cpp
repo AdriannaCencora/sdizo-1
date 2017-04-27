dc6fdd36187a9ae84fe0e54312aa939e933474aa
Jakub Krzemiński <krzeminski.k@outlook.com>
2017-04-27 21:48:56 +0200

    Copied implementation from bst to rbtree

--- sdizo/source/structures/rbTree/rbTree.cpp
+++ sdizo/source/structures/rbTree/rbTree.cpp
@@ -1,4 +1,205 @@
 #include "stdafx.h"
 #include "rbTree.h"
 
+void rbTree::addElement(int value)
+{
+}
 
+void rbTree::removeElement(int value)
+{
+	Node* toDelete = getNode(root.get(), value);
+	if (toDelete != nullptr)
+	{
+		removeNode(toDelete);
+		--size;
+	}
+}
+
+void rbTree::clearStructure()
+{
+	root.reset();
+	size = 0;
+}
+
+bool rbTree::findValue(int toFind)
+{
+	Node* searchPoint = root.get();
+	while (searchPoint != nullptr && searchPoint->value != toFind)
+	{
+		if (toFind < searchPoint->value)
+			searchPoint = searchPoint->left.get();
+		else
+			searchPoint = searchPoint->right.get();
+	}
+	return (searchPoint != nullptr);
+}
+
+void rbTree::printData()
+{
+	printNode(string(""), string(""), root);
+}
+
+void rbTree::rotateLeft(Node * axis)
+{
+	if (axis->right == nullptr)
+		return;
+
+	bool isAxisLeftChild = isLeftChild(axis);
+
+	//Creating hooks
+	unique_ptr<rbTree::Node>* axisUniquePtr = getUniqueNode(axis);
+	unique_ptr<rbTree::Node> axisUnique = move(*axisUniquePtr);
+
+	Node* rightChild = axis->right.get();
+	unique_ptr<rbTree::Node>* rightChildUniquePtr = getUniqueNode(rightChild);
+	unique_ptr<rbTree::Node> rightChildUnique = move(*rightChildUniquePtr);
+
+	//Moving childs
+	if (rightChild->left != nullptr)
+	{
+		axis->right = move(rightChild->left);
+		axis->right->parent = axis;
+	}
+	rightChild->left = move(axisUnique);
+
+	//Resettings parents child pointers
+	if (axis->parent != nullptr)
+	{
+		if (isAxisLeftChild)
+			axis->parent->left = move(rightChildUnique);
+		else
+			axis->parent->right = move(rightChildUnique);
+	}
+
+	//Setting parents
+	rightChild->parent = axis->parent;
+	axis->parent = rightChild;
+
+	//Resetting root to correct place if needed
+	if (rightChild->parent == nullptr)
+		root = move(rightChildUnique);
+}
+
+void rbTree::rotateRight(Node * axis)
+{
+	if (axis->left == nullptr)
+		return;
+
+	bool isAxisLeftChild = isLeftChild(axis);
+
+	//Creating hooks
+	unique_ptr<rbTree::Node>* axisUniquePtr = getUniqueNode(axis);
+	unique_ptr<rbTree::Node> axisUnique = move(*axisUniquePtr);
+
+	Node* leftChild = axis->left.get();
+	unique_ptr<rbTree::Node>* leftChildUniquePtr = getUniqueNode(leftChild);
+	unique_ptr<rbTree::Node> leftChildUnique = move(*leftChildUniquePtr);
+
+	//Moving childs
+	if (leftChild->right != nullptr)
+	{
+		axis->left = move(leftChild->right);
+		axis->left->parent = axis;
+	}
+	leftChild->right = move(axisUnique);
+
+	//Resettings parents child pointers
+	if (axis->parent != nullptr)
+	{
+		if (isAxisLeftChild)
+			axis->parent->left = move(leftChildUnique);
+		else
+			axis->parent->right = move(leftChildUnique);
+	}
+
+	//Setting parents
+	leftChild->parent = axis->parent;
+	axis->parent = leftChild;
+
+	//Resetting root to correct place if needed
+	if (leftChild->parent == nullptr)
+		root = move(leftChildUnique);
+}
+
+rbTree::Node * rbTree::getNode(Node * startPoint, int value)
+{
+	while (startPoint != nullptr && startPoint->value != value)
+	{
+		if (value < startPoint->value)
+			startPoint = startPoint->left.get();
+		else
+			startPoint = startPoint->right.get();
+	}
+	return startPoint;
+}
+
+unique_ptr<rbTree::Node>* rbTree::getUniqueNode(Node * toGet)
+{
+	if (toGet->parent == nullptr)
+		return &root;
+	Node* parent = toGet->parent;
+	if (parent->left.get() == toGet)
+		return &(parent->left);
+	else
+		return &(parent->right);
+}
+
+rbTree::Node * rbTree::getMin(Node * searchPoint)
+{
+	while (searchPoint->left != nullptr)
+		searchPoint = searchPoint->left.get();
+	return searchPoint;
+}
+
+rbTree::Node * rbTree::getMax(Node * searchPoint)
+{
+	while (searchPoint->right != nullptr)
+		searchPoint = searchPoint->right.get();
+	return searchPoint;
+}
+
+bool rbTree::isLeftChild(Node * child)
+{
+	if (child->parent == nullptr)
+		return false;
+
+	Node* parent = child->parent;
+	return (parent->left.get() == child);
+}
+
+bool rbTree::isRightChild(Node * child)
+{
+	if (child->parent == nullptr)
+		return false;
+
+	Node* parent = child->parent;
+	return (parent->right.get() == child);
+}
+
+void rbTree::removeNode(Node * toDelete)
+{
+}
+
+void rbTree::printNode(std::string & sMiddle, std::string & sBefore, unique_ptr<Node>& currNode)
+{
+	string s;
+	if (currNode != nullptr)
+	{
+		s = sMiddle;
+		if (sBefore == (string(1, (char)47) + string(1, (char)126)))
+			s[s.length() - 2] = ' ';
+		printNode(s + (char)124 + " ", (string(1, (char)47)) + string(1, (char)126), currNode->right);
+
+		s = s.substr(0, sMiddle.length() - 2);
+
+		if (currNode->color == Color::Red)
+			cout << s << sBefore << "(" << currNode->value << ")" << endl;
+		else
+			cout << s << sBefore << currNode->value << endl;
+
+		s = sMiddle;
+		if (sBefore == (string(1, (char)92)) + string(1, (char)126))
+			s[s.length() - 2] = ' ';
+		printNode(s + (char)124 + " ", (string(1, (char)92)) + string(1, (char)126), currNode->left);
+	}
+}
--- sdizo/source/structures/rbTree/rbTree.h
+++ sdizo/source/structures/rbTree/rbTree.h
@@ -7,5 +7,37 @@ class rbTree :
 public:
 	rbTree() = default;
 	virtual ~rbTree() = default;
+
+	virtual void addElement(int value);
+	virtual void removeElement(int value);
+	virtual void clearStructure();
+	virtual bool findValue(int toFind);
+	virtual void printData();
+
+private:
+
+	enum Color { Black, Red };
+
+	struct Node {
+		Node* parent = nullptr;
+		unique_ptr<Node> right = nullptr;
+		unique_ptr<Node> left = nullptr;
+		int	  value;
+		Color color;
+	};
+
+	unique_ptr<Node> root = nullptr;
+	unsigned int size = 0;
+
+	void rotateLeft(Node* axis);
+	void rotateRight(Node* axis);
+	Node* getNode(Node* startPoint, int value);
+	unique_ptr<Node>* getUniqueNode(Node* toGet);
+	Node* getMin(Node* searchPoint);
+	Node* getMax(Node* searchPoint);
+	bool isLeftChild(Node* child);
+	bool isRightChild(Node* child);
+	void removeNode(Node* toDelete);
+	void printNode(std::string &sMiddle, std::string &sBefore, unique_ptr<Node> &currNode);
 };
 
